8fd0de3a8d6fbd840d254fdaa9640cae531d9d27
irb1s74 <kolyirb1s@gmail.com>
2022-04-10 15:57:50 +0500

    Commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,17 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { UserCreateDto } from 'src/users/dto/UserCreate.dto';
 import { UserLoginDto } from 'src/users/dto/UserLogin.dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {
-  }
+  constructor(private authService: AuthService) { }
 
   @Post('/login')
   @UsePipes(ValidationPipe)
